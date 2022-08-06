205d451079e412d4cc17741ad1f0b864b1f66c33
agalecki <agalecki@umich.edu>
2022-08-06 12:46:50 -0400

    ??

--- _01process_Rmd.R
+++ _01process_Rmd.R
@@ -4,7 +4,7 @@ source("Rfunctions/process_Rmd.R")
 
 # In general `Rmd_names` is a character _vector_
 # that contains the nmaes of Rmd files
-Rmd_names <- "90-test-chunks"
+Rmd_names <- c("11-data", "90-test-chunks")
 lapply(as.list(Rmd_names), process_Rmd)
  
 
