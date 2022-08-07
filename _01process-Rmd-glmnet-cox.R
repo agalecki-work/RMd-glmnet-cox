61696d8157aac05a75825a7aef9f25a8d5f3d815
agalecki <agalecki@umich.edu>
2022-08-07 15:18:45 -0400

    varia

--- _01process-Rmd-glmnet-cox.R
+++ _01process-Rmd-glmnet-cox.R
@@ -1,13 +1,12 @@
 # Make sure you are running R program in correct working directory
 getwd()
 
-# source("_01process_Rmd.R")
+# source("_01process-Rmd-glmnet-cox.R")
 source("Rfunctions/process_Rmd.R") # Function process_Rmd
 
 # In general `Rmd_names` is a character _vector_
 # that contains the nmaes of Rmd files
 Rmd_names <- c("11-data", "95-test-chunks")
-Rmd_names <- c("95-test-chunks")
 lapply(as.list(Rmd_names), process_Rmd)
  
 
